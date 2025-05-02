6d11fc967a3781946decb26e1a496b8bf346bac5
AlexFerreira10 <correaferreiraalex92@gmail.com>
2025-05-01 21:41:00 -0300

    feat: create a project

---
+++ src/main/java/com/economizei/api/EconomizeiApiApplication.java
@@ -0,0 +1,13 @@
+package com.economizei.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomizeiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomizeiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/economizei/api/EconomizeiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.economizei.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomizeiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
